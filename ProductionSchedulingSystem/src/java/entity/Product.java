465437f6ce396f1469481cedcf8420f81b8932f5
Thanh <cnguyenphuongthanh@gmail.com>
2024-10-26 16:35:11 +0700

    init project and establish connection to database

---
+++ ProductionSchedulingSystem/src/java/entity/Product.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+
+public class Product {
+    private int pId;
+    private String pName;
+    private String description;
+
+    public Product() {
+    }
+
+    public Product(int pId, String pName, String description) {
+        this.pId = pId;
+        this.pName = pName;
+        this.description = description;
+    }
+
+    public int getpId() {
+        return pId;
+    }
+
+    public void setpId(int pId) {
+        this.pId = pId;
+    }
+
+    public String getpName() {
+        return pName;
+    }
+
+    public void setpName(String pName) {
+        this.pName = pName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    
+}
