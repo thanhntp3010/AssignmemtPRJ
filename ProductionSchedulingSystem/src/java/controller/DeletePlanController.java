1745f8483fbfd4e0676c57fe5398ad79dcfcd8c3
Thanh <cnguyenphuongthanh@gmail.com>
2024-11-05 00:02:40 +0700

    fix bug delete plan

--- ProductionSchedulingSystem/src/java/controller/DeletePlanController.java
+++ ProductionSchedulingSystem/src/java/controller/DeletePlanController.java
@@ -4,6 +4,7 @@
  */
 package controller;
 
+import dao.PlanDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -26,18 +27,11 @@ public class DeletePlanController extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet DeletePlanController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet DeletePlanController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        int id = Integer.parseInt(request.getParameter("id"));
+        PlanDAO d = new PlanDAO();
+        d.deleteById(id);
+        
+        response.sendRedirect("managePlan");
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
